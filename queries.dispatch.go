4d9a95a6587339279f9543fcd3ba17114956b032
Alessandro Tagliani <alessandro.tagliani@tuta.io>
2025-12-12 20:16:54 +0100

    perf(dispatch): not ignoring tasks with version as empty string

--- queries.dispatch.go
+++ queries.dispatch.go
@@ -38,7 +38,7 @@ SELECT *
 FROM task_scheduled_v2
 WHERE
 	dispatch_after <= $1 AND
-	(version <= $2 OR version IS NULL)
+	(version <= $2 OR version IS NULL OR version = '')
 ORDER BY
 	priority DESC,
 	dispatch_after ASC
